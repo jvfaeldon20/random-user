8c1765b1cfa505449bde5d9c80231c57d6ec1443
JV Faeldon <jonelfaeldon@gmail.com>
2023-07-27 07:39:41 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import RootLayout from './layouts/RootLayout'
+import UserDisplay from './components/UserDisplay'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout/>}>
+      <Route 
+        index 
+        element={<UserDisplay/>}
+      ></Route>
+    </Route>
+  )
+)
+
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>
 }
 
-export default App;
+export default App
---
+++ src/components/UserDisplay.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+import { Layout, theme, Spin, Button } from 'antd';
+
+const { Content } = Layout;
+
+const UserDisplay = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken()
+
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true);
+  
+  const fetchRandomUser = async () => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      const response = await axios.get('https://randomuser.me/api');
+      const userData = response.data.results[0];
+      setUser(userData);
+      setLoading(false);
+
+      // Save the user data to local storage
+      localStorage.setItem('randomUser', JSON.stringify(userData));
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
+  };
+
+  const handleRefresh = async () => {
+    setLoading(true);
+    await fetchRandomUser();
+  };
+
+  useEffect(() => {
+    // Check if user data exists in local storage, if not fetch a new user
+    const savedUser = localStorage.getItem('randomUser');
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+      setLoading(false);
+    } else {
+      fetchRandomUser();
+    }
+  }, [])
+
+  return (
+    <Layout className="layout">
+      <Content
+        style={{
+          padding: '50px',
+        }}
+      >
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+            textAlign: 'center',
+          }}
+        >
+          <Content style={{ padding: '24px', minHeight: '280px' }}>
+              {loading ? (
+                <div style={{ textAlign: 'center' }}>
+                  <Spin size="large" />
+                </div>
+              ) : (
+                <div>
+                  {user ? (
+                    <div>
+                      <h2>{`${user.name.first} ${user.name.last}`}</h2>
+                      <p>Email: {user.email}</p>
+                      <Button 
+                        onClick={handleRefresh}
+                        type='primary'
+                      >Refresh</Button>
+                    </div>
+                  ) : (
+                    <p>No user data available.</p>
+                  )}
+                </div>
+              )}
+            </Content>
+        </div>
+      </Content>
+    </Layout>
+  );
+};
+
+export default UserDisplay;
